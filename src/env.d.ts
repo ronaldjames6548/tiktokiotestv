cd67f9aa927c1059a597136316f911865fb88994
ronaldjames6548 <ronaldjames6548@gmail.com>
2025-12-24 17:36:55 +0500

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,9 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+// Extend Astro's App.Locals interface to include our custom properties
+declare namespace App {
+  interface Locals {
+    shouldIndex?: boolean;
+  }
+}
